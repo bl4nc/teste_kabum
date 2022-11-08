dfadcbabebcc89ba6f5e8caf0f31ddf67385b8d0
Rafael Araújo Rocha <40602200+bl4nc@users.noreply.github.com>
2022-11-08 20:44:13 -0300

    Build

---
+++ build/mock/DeliveryCompanies.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deliveries_companies = void 0;
+exports.deliveries_companies = [
+    {
+        name: 'Entrega Ninja',
+        rules: {
+            calculation_constant: 0.3,
+            min_height: 10,
+            max_height: 200,
+            min_width: 6,
+            max_width: 140,
+            deadline: 6
+        }
+    },
+    {
+        name: 'Entrega Kabum',
+        rules: {
+            calculation_constant: 0.2,
+            min_height: 5,
+            max_height: 140,
+            min_width: 13,
+            max_width: 125,
+            deadline: 4
+        }
+    }
+];
+//# sourceMappingURL=DeliveryCompanies.js.map
---
+++ build/utils/calculate-delivery.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.calculateDelivery = void 0;
+function calculateDelivery(weight, calculation_constant) {
+    return (weight * calculation_constant) / 10;
+}
+exports.calculateDelivery = calculateDelivery;
+//# sourceMappingURL=calculate-delivery.js.map
