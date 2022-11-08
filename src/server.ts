0dc7177a445f161d634ac2f4f3d104d1108c6b74
Rafael Araújo Rocha <40602200+bl4nc@users.noreply.github.com>
2022-11-08 19:21:06 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,6 @@ app.use(cors())
 
 app.use(routes);
 
-app.listen(process.env.PORT ?? 3000, () => console.log(`Server is run ${process.env.PORT ?? 3000}`));
+app.listen(process.env.PORT || 3000, () => console.log(`Server is run ${process.env.PORT || 3000}`));
 
 export default app 
