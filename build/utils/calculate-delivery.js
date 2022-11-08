fa0cb2f160e60edfa2e324d9f804ae05b1a468a2
Rafael Araújo Rocha <40602200+bl4nc@users.noreply.github.com>
2022-11-08 20:46:10 -0300

    Remove build

--- build/utils/calculate-delivery.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.calculateDelivery = void 0;
-function calculateDelivery(weight, calculation_constant) {
-    return (weight * calculation_constant) / 10;
-}
-exports.calculateDelivery = calculateDelivery;
-//# sourceMappingURL=calculate-delivery.js.map
